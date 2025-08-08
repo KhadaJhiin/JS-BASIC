f6516255c9fb7a485ef987b40d0fed3cb28d5af2
Jose <josemiguelortegagaviria@gmail.com>
2025-08-08 18:00:42 -0500

    advanced objects and classes done

--- Intermediate/05-advanced-classes.js
+++ Intermediate/05-advanced-classes.js
@@ -109,3 +109,64 @@ console.log(session1 === session2)
 const session3 = new Session("MoureDev")
 console.log(session3.name)
 console.log(session2 === session3)
+
+// --------------------------------------------------------SYMBOL
+
+const ID = Symbol("id")
+
+class User {
+    constructor(name) {
+        this.name = name
+        this[ID] = Math.random()
+    }
+
+    getId() {
+        return this[ID]
+    }
+}
+
+const user = new User("Brais")
+console.log(user.name)
+console.log(user.ID)
+console.log(user.getId())
+
+// --------------------------------------------------------instanceof
+
+class Car { }
+
+const car = new Car()
+
+console.log(car instanceof Car)
+
+// --------------------------------------------------------create
+
+const anotherCar = Object.create(Car.prototype)
+
+console.log(anotherCar instanceof Car)
+
+// --------------------------------------------------------Proxy
+
+const proxy = {
+    get(target, property) {
+        console.log(`Se accede a la propiedad ${property}`)
+        return target[property]
+    },
+    set(target, property, value) {
+        if (property === "balance" && value < 0) {
+            throw new Error("El saldo no puede ser negativo")
+        }
+        target[property] = value
+    }
+}
+
+class BankAccount {
+    constructor(balance) {
+        this.balance = balance
+    }
+}
+
+const account = new Proxy(new BankAccount(100), proxy)
+console.log(account.balance)
+
+account.balance = 50
+console.log(account.balance)
---
+++ Intermediate/06-advanced-objects-classes-exercises.js
@@ -0,0 +1,111 @@
+// 1. Add a function to the prototipe of an object
+// 2. Create an object that inherits of another
+// 4. Uses get and set in an object
+
+let device = {
+    type : "Electronic",
+    brand : "Sansung",
+    price : 1000,
+    get info(){
+        return `${this.type} - ${this.brand} - ${this.price}`;
+    }
+}
+
+device.getBrand = function(){
+    return this.brand;
+};
+
+let cellPhone = Object.create(device);
+console.log(cellPhone.getBrand());
+console.log(device.info);
+
+
+// 3. Define a method of instance in an object
+
+function Computer (brand, price){
+    this.brand = brand;
+    this.price = price;
+};
+
+Computer.prototype.getPrice = function(){
+    console.log(`This device is ${this.price}`);
+};
+
+let laptopOne = new Computer("Lenovo", 2000);
+laptopOne.getPrice();
+
+// 5. Use the assign operation in an object
+
+let landAnimals = {
+    habit : "Selva",
+    legs : 8,
+};
+
+let aquaticAnimals = {
+    habit : "Ocean",
+    fins : 2,
+};
+
+let entireAnimals = Object.assign(landAnimals,aquaticAnimals);
+console.log(entireAnimals);
+
+// 6. Create an abstract classe
+
+class Employed {
+    constructor(name , salary) {
+        if(new.target === Employed){
+            throw new Error(`An abstract class cannot be instantiaed`);
+        };
+        this.name = name;
+        this.salary = salary;
+    };
+};
+
+// 7. Use polimorfism in two different classes
+
+class ParTimeEmployed extends Employed {
+    constructor(name, salary, workHours){
+        super(name, salary);
+        this.workHours = workHours;
+    };
+
+    generatedMoney(){
+        return this.workHours * this.salary;
+    };
+};
+
+let firstEmployed = new ParTimeEmployed("Jose",1000,5);
+console.log(firstEmployed.generatedMoney());
+
+class FullTimeEmployed extends Employed {
+    constructor(name,salary) {
+        super(name,salary);
+    };
+
+    generatedMoney(){
+        return this.salary;
+    };
+};
+
+let employed = new FullTimeEmployed("Leo",15000);
+console.log(employed.generatedMoney());
+
+// 8. Implements a Mixin
+
+let dayOff = {
+    getDayOff(){
+        console.log(`Hi ${this.name} you have just 1 day per week`);
+    }
+}
+
+Object.assign(FullTimeEmployed.prototype, dayOff);
+Object.assign(ParTimeEmployed.prototype, dayOff);
+
+firstEmployed.getDayOff();
+employed.getDayOff();
+
+// 9. Create a singleton
+
+
+
+// 10. Develop a proxy
